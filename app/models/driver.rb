5a1c35be4b60c3c114894c206179e0f9fd819204
Josey Kinnaman <josey.kinnaman@planetargon.com>
2023-02-19 14:31:56 -0800

    Organize models

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,4 @@
 class Driver < ApplicationRecord
+  # ASSOCIATIONS
   has_many :rides
 end
--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,11 +1,22 @@
 class Ride < ApplicationRecord
+
+  # ASSOCIATIONS
   belongs_to :driver
+
+  # CALLBACKS
   before_validation :calculate_score, unless: :score?
+
+  # VALIDATIONS
   validates :score, :ride_earnings, :commute_duration, :ride_duration, presence: true
 
-  # Calculate the score of a ride in $ per hour as: (ride earnings) / (commute duration + ride duration).
+  # SCOPES
+  scope :by_score, -> { order(score: :desc) }
+
+  # CLASS METHODS
+
   def calculate_score
-    # 70 / (334 + 395) 729 seconds OR 12.15 minutes OR 0.2025 hours
+    # Calculate the score of a ride in $ per hour as: (ride earnings) / (commute duration + ride duration).
+    # EX: 70 / (334 + 395) 729 seconds OR 12.15 minutes OR 0.2025 hours
     self.score = ride_earnings / ((commute_duration + ride_duration) / 3600.0)
   end
 end
