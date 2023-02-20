469e4073288e477bc193051b936b2c876dfde421
Josey Kinnaman <josey.kinnaman@planetargon.com>
2023-02-19 16:17:56 -0800

    Add tests

--- spec/models/driver_spec.rb
+++ spec/models/driver_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Driver, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many(:rides) }
+  end
 end
